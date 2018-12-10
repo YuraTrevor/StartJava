f9e7809e7f2d030c6facdb010913701f8c7f9127
Yura Trevor <king.3017@mail.ru>
2018-12-10 20:16:44 +0300

    implement H_1

---
+++ Lesson_1/Variable.java
@@ -0,0 +1,22 @@
+public class Variable {	
+    public static void main(String[] args) {
+        String monitor = "ASUS MX32VQ";
+        byte hdd = 127;
+        int videoCard = 1050;
+        long sdd = 2000;
+        double ram = 16.0;
+        float frequency = 3.7f;
+        char keyboard = '\u2328';
+        boolean compares = sdd > hdd;
+        
+        System.out.println("My Monitor:" + monitor);
+        System.out.println("My Disk C:" + hdd + " Gigabyte");
+        System.out.println("My NVIDIA GeForce GTX:" + videoCard + " ti");
+        System.out.println("My Disk D:" + sdd + " Gigabyte");
+        System.out.println("My ram:" + ram + " Gigabyte");
+        System.out.println("My processor:" + frequency + " GHz");
+        System.out.println("My " + keyboard );
+        System.out.println(true);
+
+    }
+}
