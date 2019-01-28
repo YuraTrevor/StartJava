b633fc09ba19cf88f718fc13bbebacc333fa0286
Yura Trevor <king.3017@mail.ru>
2019-01-28 17:48:53 +0300

    unicode

---
+++ Lesson_1/Unicode.java
@@ -0,0 +1,13 @@
+public class Unicode {
+    public static void main(String[] args) {
+
+        char a = 'Ⓐ';
+        char b = '⟂';
+        while (true) {
+            System.out.println(a);
+            System.out.print(b);
+            break;
+        }
+
+    }
+}
